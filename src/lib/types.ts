31b54acbb74bbd3198223d5b7cda93625de96732
nakanoyuki <yukich41@gmail.com>
2025-03-04 09:42:42 +0900

    WIP: 記事の型定義

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -43,41 +43,11 @@ export type ProjectFrontmatter = {
   filename: string;
 };
 
-export type ArticleFrontmatter = {
-  /**
-   * The title of the article
-   */
+export type ArticleType = {
+  id: number;
   title: string;
-
-  /**
-   * THe summary description of the article
-   */
   description: string;
-
-  /**
-   * The tags of the article
-   * (eg. ["JavaScript", "React", "Node.js"])
-   */
-  tags?: string[];
-
-  /**
-   * The estimated time to read the article in minutes
-   */
-  time: number;
-
-  /**
-   * Whether the article should be featured on the homepage
-   */
-  featured: boolean;
-
-  /**
-   * The timestamp the article was published in W3C format
-   */
-  timestamp: string;
-
-  /**
-   * The URL of the article on the website
-   * (eg. https://zaggonaut.dev/blog/my-article)
-   */
-  filename: string;
+  url: string;
+  likes: string;
+  created_at: string;
 };
