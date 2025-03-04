c2d7f0abb969ff2d9677f34bf325affe1d9ba9f6
nakanoyuki <yukich41@gmail.com>
2025-03-04 12:02:44 +0900

    不要な関数の削除

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -1,36 +1,12 @@
-import type { ArticleFrontmatter, ProjectFrontmatter } from "./types";
+import type { ProjectFrontmatter } from "./types";
 import { getShortDescription, processContentInDir } from "./utils";
-
-export const articles = (
-  await processContentInDir<ArticleFrontmatter, ArticleFrontmatter>(
-    "blog",
-    (data) => {
-      const shortDescription = getShortDescription(
-        data.frontmatter.description,
-      );
-      return {
-        title: data.frontmatter.title,
-        description: shortDescription,
-        tags: data.frontmatter.tags,
-        time: data.frontmatter.time,
-        featured: data.frontmatter.featured,
-        timestamp: data.frontmatter.timestamp,
-        filename: `/blog/${data.frontmatter.filename}`,
-      };
-    },
-  )
-).sort((a, b) => {
-  const dateA = new Date(a.timestamp);
-  const dateB = new Date(b.timestamp);
-  return dateB.getTime() - dateA.getTime();
-});
 
 export const projects = (
   await processContentInDir<ProjectFrontmatter, ProjectFrontmatter>(
     "projects",
-    (data) => {
+    data => {
       const shortDescription = getShortDescription(
-        data.frontmatter.description,
+        data.frontmatter.description
       );
       return {
         title: data.frontmatter.title,
@@ -42,7 +18,7 @@ export const projects = (
         timestamp: data.frontmatter.timestamp,
         filename: `/projects/${data.frontmatter.filename}`,
       };
-    },
+    }
   )
 ).sort((a, b) => {
   const dateA = new Date(a.timestamp);
